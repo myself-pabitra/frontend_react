ccf7958029f30280fa87a1ddf2822daa99d7fd53
pabitra <pabitrapandit2001@gmail.com>
2023-06-07 17:56:58 +0530

    2nd commit

---
+++ src/components/Socialmedia.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { bsTwitter, BsInstagram, BsLinkedin, BsFacebook, BsGithub } from 'react-icons/bs'
+const Socialmedia = () => {
+    return (
+        <div className='app__social'>
+            <div><BsLinkedin /></div>
+            <div><BsGithub /></div>
+            <div><BsInstagram /></div>
+            <div><BsFacebook /></div>
+
+
+        </div>
+    )
+}
+
+export default Socialmedia
+
